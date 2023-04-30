230665dba5dcfb57cb3250700d249b3e3fe55e75
zarrar1607 <zarrar1607@gmail.com>
2023-04-30 12:08:27 -0500

    commit

--- main.py
+++ main.py
@@ -47,23 +47,41 @@ from collections import deque
 # Save the Data that was created on parsing earlier
 # Load Data
 # search -> Querry
+# Define an empty list to store the URLs to visit
 url_frontier = []
+
+# Define a set to store the URLs that have already been visited
 url_visited = set()
+
+# Define the niche_crawl function that takes a URL and a maximum depth as arguments
 def niche_crawl(url, max_depth=1):
+    
+    # Check if we have reached the maximum depth, and return if so
     if not max_depth:
         return
     
+    # Check if the URL has not been visited before, and add it to the frontier if so
     if url not in url_visited:
         url_frontier.append(url)
     
+    # Get all the links on the current page
     links = get_links(url=url)
     
+    # Iterate over the links
     for link in links:
+        
+        # Check if the link has already been visited, and continue to the next link if so
         if link in url_visited:
             continue
+        
+        # Add the link to the visited set
         url_visited.add(link)
+        
+        # Add the link to the frontier if it hasn't already been added
         if link not in url_frontier:
             url_frontier.append(link)
+    
+        # Recursively call the niche_crawl function with the new link and a depth of one less than the current depth
         niche_crawl(url=link, max_depth=max_depth-1)
     
 
